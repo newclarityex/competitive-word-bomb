63d31ce61eb9a571902ac3b57db4f74ed1b8d7bb
newclarityex <69740123+newclarityex@users.noreply.github.com>
2021-10-25 18:55:42 -0400

    Use port variable

--- js/websockets.js
+++ js/websockets.js
@@ -1,5 +1,5 @@
 const port = 80;
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket("ws://localhost:" + port);
 
 socket.addEventListener("open", (event) => {
     console.log("Successfully connected!");
