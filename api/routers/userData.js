ec873cd39edaad983319be3a8950672d47359bdc
newclarityex <69740123+newclarityex@users.noreply.github.com>
2021-10-30 23:22:10 -0400

    semantics

--- api/routers/userData.js
+++ api/routers/userData.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.get("/user", (req, res) => {
-    let user = req.query.username;
+    let username = req.query.username;
 
-    User.findOne({username: user}, "username elo admin", {}, (err, user) => {
+    User.findOne({username: username}, "username elo admin", {}, (err, user) => {
         if (err){
             res.sendStatus(500)
             return;
