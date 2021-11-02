cd1dfcbc1b9339e9c4ca053a19845a0c7f4965c7
TheEggShark <theeggshark@gmail.com>
2021-11-02 11:34:42 -0500

    didnt acually add the file

--- api/routers/userData.js
+++ api/routers/userData.js
@@ -3,14 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.get("/user", (req, res) => {
-<<<<<<< HEAD
-    let username = req.query.username;
-
-    User.findOne({username: username}, "username elo admin", {}, (err, user) => {
-=======
     let user = req.query.username;
     User.findOne({username: user}, "username elo admin", {}, (err, user) => {
->>>>>>> 61da5714cf3a04177001ec1808405612db786c57
         if (err){
             res.sendStatus(500)
             return;
