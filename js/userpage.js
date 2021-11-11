6a4bc0067c5705274f4dcbb31fd8a4e84d14b962
TheEggShark <ther3al3ggshark@gmail.com>
2021-11-11 09:31:24 -0600

    backend baics

--- js/userpage.js
+++ js/userpage.js
@@ -16,19 +16,37 @@ function userBox(data) {
     }
     else {
         console.log(data)
-        var userBox = document.getElementsByClassName("user-box")[0];
-        var nameBox = userBox.getElementsByClassName("user-name")[0];
-        var dateArea = userBox.getElementsByClassName("date")[0];
-        var eloArea = userBox.getElementsByClassName("elo")[0];
-        var rankArea = userBox.getElementsByClassName("ranking")[0];
+        const userBox = document.getElementsByClassName("user-box")[0];
+        const nameBox = userBox.getElementsByClassName("user-name")[0];
+        const dateArea = userBox.getElementsByClassName("date")[0];
+        const eloArea = userBox.getElementsByClassName("elo")[0];
+        const rankArea = userBox.getElementsByClassName("ranking")[0]; // need to rember the 
         //position number for ranks would be right here but we dont have that yet
-        var gameWin = document.getElementById("gamesWon");
-        var gameLost = document.getElementById("gamesLost");
-        var gamePlay = document.getElementById("gamesPlayed");
-        var winsRate = document.getElementById("winRate")
+        const progressline = userBox.getElementsByClassName("position-progress")[0];
+        const gameWin = document.getElementById("gamesWon");
+        const gameLost = document.getElementById("gamesLost");
+        const gamePlay = document.getElementById("gamesPlayed");
+        const winsRate = document.getElementById("winRate")
         /* jesus there is going to be alot of var 
         TODO when ranks get added add it */
-        
+        let joinDate = dateFormater(data.dateJoined);
+
+        nameBox.innerText = data.username;
+        dateArea.innerText = joinDate;
+        eloArea.innerText = data.elo;
+        progressline.setAttribute("style", `--progress: ${(data.elo/2500)*100}%`);
+        gameWin.innerText = data.rankedWon;
+        gameLost.innerText = data.rankedLost;
+        gamePlay.innerText = data.rankedPlayed;
+        winsRate.innerText = (data.rankedWon/data.rankedPlayed)*100;
+        console.log((0/0)*100)
+
         userBox.classList.remove("hidden");
     }
+}
+
+function dateFormater(string){
+    let joinDate = new Date(string);
+    const mmddyyyy = [joinDate.getUTCMonth() + 1, joinDate.getUTCDate(), joinDate.getUTCFullYear()];
+    return mmddyyyy.join("/");
 }
