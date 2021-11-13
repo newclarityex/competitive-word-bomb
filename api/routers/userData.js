337a6bd49e2fb6e3bd56bcf763b83812defb1928
newclarityex <69740123+newclarityex@users.noreply.github.com>
2021-11-13 18:16:20 -0500

    fixing overlapping variables

--- api/routers/userData.js
+++ api/routers/userData.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.get("/user", (req, res) => {
-    let user = req.query.username;
-    User.findOne({username: user}, "username elo admin gamesPlayed rankedPlayed rankedWon rankedLost dateJoined", {}, (err, user) => {
+    let username = req.query.username;
+    User.findOne({username: username}, "username elo admin gamesPlayed rankedPlayed rankedWon rankedLost dateJoined", {}, (err, user) => {
         if (err){
             res.sendStatus(500)
             return;
